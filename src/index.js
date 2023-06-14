7f88f793b819bdfdfdcafa63b27cbdfcd6c3a366
dafibh <amarghadafi@gmail.com>
2023-06-14 23:52:20 +0800

    v2.2.0

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import App from 'App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { MaterialUIControllerProvider } from 'context/md';
 
-createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-        <MaterialUIControllerProvider>
-            <App />
-        </MaterialUIControllerProvider>
-    </BrowserRouter>
+const container = document.getElementById("app");
+const root = createRoot(container);
+
+root.render(
+  <BrowserRouter>
+    <MaterialUIControllerProvider>
+      <App />
+    </MaterialUIControllerProvider>
+  </BrowserRouter>
 );
