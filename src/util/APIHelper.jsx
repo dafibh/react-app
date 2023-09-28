3f146712265a5ddef4b0a714e7448d0cafb95e0b
dafibh <amarghadafi@gmail.com>
2023-09-28 19:59:54 +0800

    use env

--- src/util/APIHelper.jsx
+++ src/util/APIHelper.jsx
@@ -1,8 +1,7 @@
 import axios from "axios";
-import config from "../config";
 
 const instance = axios.create({
-  baseURL: config.WS_BASE_URL,
+  baseURL: process.env.REACT_APP_API_URL
 });
 
 export const getAnalyticsSalesCountry = async () =>
