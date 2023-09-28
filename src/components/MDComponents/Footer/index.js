7cbef71b80991d758afae452b151c9eecf887a5d
dafibh <amarghadafi@gmail.com>
2023-09-28 19:40:21 +0800

    Footer

--- src/components/MDComponents/Footer/index.js
+++ src/components/MDComponents/Footer/index.js
@@ -3,7 +3,6 @@ import PropTypes from "prop-types";
 
 // @mui material components
 import Link from "@mui/material/Link";
-import Icon from "@mui/material/Icon";
 
 // Material Dashboard 2 PRO React components
 import MDBox from "components/MDBase/MDBox";
@@ -45,19 +44,13 @@ function Footer({ company, links }) {
         fontSize={size.sm}
         px={1.5}
       >
-        &copy; {new Date().getFullYear()}, made with
-        <MDBox fontSize={size.md} color="text" mb={-0.5} mx={0.25}>
-          <Icon color="inherit" fontSize="inherit">
-            favorite
-          </Icon>
-        </MDBox>
-        by
+        &copy; {new Date().getFullYear()}
         <Link href={href} target="_blank">
           <MDTypography variant="button" fontWeight="medium">
             &nbsp;{name}&nbsp;
           </MDTypography>
         </Link>
-        for a better web.
+        All Rights Reserved.
       </MDBox>
       <MDBox
         component="ul"
@@ -84,12 +77,11 @@ function Footer({ company, links }) {
 
 // Setting default values for the props of Footer
 Footer.defaultProps = {
-  company: { href: "https://www.creative-tim.com/", name: "Creative Tim" },
+  company: { href: "https://www.example.com/", name: "example.com" },
   links: [
-    { href: "https://www.creative-tim.com/", name: "Creative Tim" },
-    { href: "https://www.creative-tim.com/presentation", name: "About Us" },
-    { href: "https://www.creative-tim.com/blog", name: "Blog" },
-    { href: "https://www.creative-tim.com/license", name: "License" },
+    { href: "https://www.example.com/", name: "example.com" },
+    { href: "https://www.example.com/presentation", name: "About Us" },
+    { href: "https://www.example.com/blog", name: "Blog" },
   ],
 };
 
